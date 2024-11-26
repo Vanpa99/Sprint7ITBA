73ea59b0c6a3e591a19ca1bc75365045ef97ad51
Matias Zahn <zahnmatias14@gmail.com>
2024-11-25 21:44:33 -0300

    login required

--- ITPowerBank/clientes/urls.py
+++ ITPowerBank/clientes/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
 urlpatterns= [
-    path('info_cliente', login_required(views.infoCliente), name='info_cliente')
+    path('info_cliente', views.infoCliente, name='info_cliente')
 ]
