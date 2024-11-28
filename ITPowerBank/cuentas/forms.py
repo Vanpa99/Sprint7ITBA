a3f5deb0dd8b417ddd19ae35080902c2c25c0938
Vanpa99 <mativanpa99@gmail.com>
2024-11-27 22:26:57 -0300

    css

--- ITPowerBank/cuentas/forms.py
+++ ITPowerBank/cuentas/forms.py
@@ -1,30 +1,6 @@
 from django import forms
 from .models import Cuenta
 
-# class CuentaForm(forms.ModelForm):
-#     class Meta:
-#         model = Cuenta
-#         fields = ['saldo', 'numero', 'tipo']  # Eliminamos 'cliente' porque lo manejamos dinámicamente
-#         labels = {
-#             'numero': 'Número de cuenta',
-#             'saldo': 'Saldo inicial',
-#             'tipo': 'Tipo de cuenta',
-#         }
-#         widgets = {
-#             'numero': forms.TextInput(attrs={'class': 'inputField'}),
-#             'tipo': forms.Select(attrs={'class': 'inputField'}),
-#             'saldo': forms.NumberInput(attrs={'class': 'inputField'}),
-#         }
-
-#     cliente_nombre = forms.CharField(  # Campo solo para mostrar el cliente
-#         label="Cliente asociado",
-#         widget=forms.TextInput(attrs={'class': 'inputField', 'readonly': 'readonly'}),
-#         required=False,
-#     )
-
-from django import forms
-from .models import Cuenta
-
 class CuentaForm(forms.ModelForm):
     cliente_nombre = forms.CharField(  # Campo adicional para mostrar el cliente
         label="Cliente asociado",
